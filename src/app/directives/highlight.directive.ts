f3d4ca72ef9f09c4b2372c12f620901b25d2e52e
luisVargasGu <vargasluis012@gmail.com>
2021-01-21 17:52:50 -0500

    Highlight directive.

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  constructor(private el: ElementRef) { }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+}
