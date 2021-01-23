3a65e25eec026d409db14157c1dbe8e6cdb0d4ce
luisVargasGu <vargasluis012@gmail.com>
2021-01-23 16:51:57 -0500

    Event emmiter for timer variable.

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
@@ -6,6 +6,8 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class ApiService {
+  // timer output
+  public functionTimer: EventEmitter<any> = new EventEmitter();
 
   constructor(
     private httpClient: HttpClient
